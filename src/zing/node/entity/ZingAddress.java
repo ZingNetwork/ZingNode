fd1b19d9240aadd308d50ce8c0a67fbe0e74676b
Ruokwok <i@ruok.cc>
2021-07-18 22:40:00 +0800

    修改 PUBLIC_KEY属性类型由String改为PublicKey

--- src/zing/node/entity/ZingAddress.java
+++ src/zing/node/entity/ZingAddress.java
@@ -1,19 +1,23 @@
 package zing.node.entity;
 
+import zing.protocol.algorithm.RSA;
+
+import java.security.PublicKey;
+
 public class ZingAddress {
 
-    private static final String HEAD = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBA";
+    public static final String HEAD = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBA";
 
-    private final String PUBLIC_KEY;
+    private final PublicKey PUBLIC_KEY;
 
     private final String ADDRESS;
 
     public ZingAddress(String address) {
         this.ADDRESS = address;
-        PUBLIC_KEY = address.replace(HEAD, "ZN");
+        PUBLIC_KEY = RSA.getPublicKey(address.replaceFirst("ZN", HEAD));
     }
 
-    public String getPublicKey() {
+    public PublicKey getPublicKey() {
         return PUBLIC_KEY;
     }
 
